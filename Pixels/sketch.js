0e01c60ba27b0e308f84062e29a12dfc20c5c01c
otshapiro <otshapiro18@gmail.com>
2017-12-11 12:47:03 -0500

    Added New Features to Filter
    Now the user can change each r,g,b indivdually

--- Pixels/sketch.js
+++ Pixels/sketch.js
@@ -1,5 +1,5 @@
 var img;
-var input, button, title;
+var input1, input2, input3,button, title;
 // loads the image first
 // going to need to change when combined with other code. 
 function  preload() {
@@ -10,15 +10,39 @@ function setup(){
   createCanvas(600,600);
     image(img,0,0);
     // This allows all the user to add an input
-    input = createInput();
-    input.position(200, 300);
+    input1 = createInput();
+    input1.position(100, 300);
     //This is the physical button   
     button = createButton('submit');
-    button.position(input.x + input.width, 300)
+    button.position(input1.x + input1.width, 300)
     button.mousePressed(change);
     //This is the prompt
-    title = createElement('h2', 'what filter?');
-    title.position(200, 300);
+    title1 = createElement('h2', 'what red');
+    title1.position(100, 250);
+    textAlign(CENTER);
+    textSize(50);
+    // This allows all the user to add an input
+    input2 = createInput();
+    input2.position(200, 300);
+    //This is the physical button   
+    button = createButton('submit');
+    button.position(input2.x + input2.width, 300)
+    button.mousePressed(change);
+    //This is the prompt
+    title2 = createElement('h2', '  blue');
+    title2.position(200, 250);
+    textAlign(CENTER);
+    textSize(50);
+    // This allows all the user to add an input
+    input3 = createInput();
+    input3.position(300, 300);
+    //This is the physical button   
+    button = createButton('submit');
+    button.position(input3.x + input3.width, 300)
+    button.mousePressed(change);
+    //This is the prompt
+    title3 = createElement('h2', ' green filter?');
+    title3.position(300, 250);
     textAlign(CENTER);
     textSize(50);
 }
@@ -30,27 +54,27 @@ function change() {
         for (var y = 0; y < img.height; y++) {
             var currentPixel = img.get(x,y);
            //This is for the r,g,b, values and if they go over or under 0 or 255
-            currentPixel[0] = currentPixel[0]+ input.value();
-            currentPixel[1] = currentPixel[1]+ input.value();
-            currentPixel[2] = currentPixel[2]+ input.value();
+            currentPixel[0] = currentPixel[0]+ input1.value();
+            currentPixel[1] = currentPixel[1]+ input2.value();
+            currentPixel[2] = currentPixel[2]+ input3.value();
             
                if (currentPixel[0]>255){
-                currentPixel[0] = currentPixel[1] - input.value();
+                currentPixel[0] = currentPixel[1] - input1.value();
             }
                if (currentPixel[0]<0){
-                currentPixel[0] = currentPixel[1] + input.value();
+                currentPixel[0] = currentPixel[1] + input1.value();
             }
                if (currentPixel[1]>255){
-                currentPixel[1] = currentPixel[1] - input.value();
+                currentPixel[1] = currentPixel[1] - input2.value();
             }
                if (currentPixel[1]<0){
-                currentPixel[1] = currentPixel[1] + input.value();
+                currentPixel[1] = currentPixel[1] + input2.value();
             }
                if (currentPixel[2]>255){
-                currentPixel[2] = currentPixel[1] - input.value();
+                currentPixel[2] = currentPixel[1] - input3.value();
             }
                if (currentPixel[2]<0){
-                currentPixel[2] = currentPixel[1] + input.value();
+                currentPixel[2] = currentPixel[1] + input3.value();
             }
             
         
